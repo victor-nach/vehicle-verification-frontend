47195ca16d65e5adb4f541bdc922f02377b30e75
victor <viheanaco@gmail.com>
2019-10-01 09:17:01 +0100

    ch: convert files to php move files to root directory and refactor #6

---
+++ includes/head.php
@@ -0,0 +1,49 @@
+
+<!-- BEGIN: Head-->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description"
+        content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords"
+        content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Plate Number Checker</title>
+    <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700"
+        rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/weather-icons/climacons.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/fonts/meteocons/style.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/morris.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/chartist.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/charts/chartist-plugin-tooltip.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.min.css">
+    <!-- END: Theme CSS-->
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/vertical-menu-modern.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/colors/palette-gradient.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/fonts/simple-line-icons/style.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/colors/palette-gradient.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/timeline.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../app-assets/css/pages/dashboard-ecommerce.min.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
+    <!-- END: Custom CSS-->
+
+</head>
+<!-- END: Head-->
---
+++ includes/nav.php
@@ -0,0 +1,29 @@
+<div class="main-menu menu-fixed menu-dark menu-accordion menu-shadow" data-scroll-to-active="true">
+        <div class="main-menu-content">
+            <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+                <li class="active"><a href="/dashboard"><span class="menu-title" data-i18n="">Dashboard </span></a>
+                </li>
+                <li class=" navigation-header"><span data-i18n="nav.category.ecommerce">Vehicles</span>
+                </li>
+                <li class=" nav-item"><a href="/verify"><span class="menu-title" data-i18n="">Verify a
+                            Vehicle</span></a>
+                </li>
+                <li class=" nav-item"><a href="/reported_vehicles"><span class="menu-title" data-i18n="">View All
+                            Reported Vehicles</span></a>
+                </li>
+                <li class=" nav-item"><a href="report.html"><span class="menu-title" data-i18n="">Report a Stolen
+                            Vehicle</span></a>
+                </li>
+                <li class=" navigation-header"><span data-i18n="nav.category.ui">Users</span>
+                </li>
+                <li class=" nav-item"><a href="new_user.html"><span class="menu-title" data-i18n="">Add a New
+                            User</span></a>
+                </li>
+                <li class=" nav-item"><a href="new_admin.html"><span class="menu-title" data-i18n="">Add a New
+                            Admin</span></a>
+                </li>
+                <li class=" nav-item"><a href="all_users.html"><span class="menu-title" data-i18n="">View All
+                            Users</span></a>
+            </ul>
+        </div>
+    </div>
